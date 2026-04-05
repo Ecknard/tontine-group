9b1660e6b595f82210d263a4a1f3a84a616b2386
Samuel_mensah <127540457+Ecknard@users.noreply.github.com>
2026-04-05 20:49:20 +0200

    Delete save.js

--- save.js
+++
@@ -1,8 +0,0 @@
-import { kv } from '@vercel/kv';
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-  const { data } = req.body;
-  await kv.set('tontine', data);
-  res.json({ ok: true });
-}
